9c1527b06799df23263e49990b183ae9204f5b2b
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-13 00:30:40 +0000

    more concise and easier to read

--- circular_queue.cpp
+++ circular_queue.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 using namespace std;
- 
-class CircularQueue{
+
+class CircularQueue {
 private:
     int size;
     int front;
@@ -11,95 +11,83 @@ private:
 public:
     CircularQueue(int size);
     ~CircularQueue();
-    bool isFull();
-    bool isEmpty();
+    bool isFull() const;
+    bool isEmpty() const;
     void enqueue(int x);
     int dequeue();
-    void display();
+    void display() const;
 };
- 
-CircularQueue::CircularQueue(int size) {
-    this->size = size;
-    front = 0;
-    rear = 0;
-    Q = new int [size];
+
+CircularQueue::CircularQueue(int size) : size(size), front(0), rear(0) {
+    Q = new int[size];
 }
- 
+
 CircularQueue::~CircularQueue() {
-    delete [] Q;
+    delete[] Q;
 }
- 
-bool CircularQueue::isEmpty() {
-    if (front == rear){
-        return true;
-    }
-    return false;
+
+bool CircularQueue::isEmpty() const {
+    return front == rear;
 }
- 
-bool CircularQueue::isFull() {
-    if ((rear + 1) % size == front){
-        return true;
-    }
-    return false;
+
+bool CircularQueue::isFull() const {
+    return (rear + 1) % size == front;
 }
- 
+
 void CircularQueue::enqueue(int x) {
-    if (isFull()){
+    if (isFull()) {
         cout << "Queue Overflow" << endl;
     } else {
         rear = (rear + 1) % size;
         Q[rear] = x;
     }
 }
- 
+
 int CircularQueue::dequeue() {
-    int x = -1;
-    if (isEmpty()){
+    if (isEmpty()) {
         cout << "Queue Underflow" << endl;
+        return -1;
     } else {
         front = (front + 1) % size;
-        x = Q[front];
+        return Q[front];
     }
-    return x;
 }
- 
-void CircularQueue::display() {
-    int i = front + 1;
-    do {
-        cout << Q[i] << flush;
-        if (i < rear) {
-            cout << " <- " << flush;
-        }
+
+void CircularQueue::display() const {
+    if (isEmpty()) {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+    int i = (front + 1) % size;
+    while (i != (rear + 1) % size) {
+        cout << Q[i] << (i != rear ? " <- " : "") << flush;
         i = (i + 1) % size;
-    } while (i != (rear + 1) % size);
-}
- 
- 
-int main() {
- 
-    int A[] = {1, 3, 5, 7, 9};
- 
-    CircularQueue cq(sizeof(A)/sizeof(A[0]) + 1);
- 
-    // Enqueue
-    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
-        cq.enqueue(A[i]);
     }
- 
+    cout << endl;
+}
+
+int main() {
+    int A[] = {1, 3, 5, 7, 9};
+    CircularQueue cq(sizeof(A) / sizeof(A[0]) + 1);
+
+    // Enqueue
+    for (int x : A) {
+        cq.enqueue(x);
+    }
+
     // Display
     cq.display();
-    cout << endl;
- 
+
     // Overflow
     cq.enqueue(10);
- 
+
     // Dequeue
-    for (int i=0; i<sizeof(A)/sizeof(A[0]); i++){
+    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++) {
         cq.dequeue();
     }
- 
+
     // Underflow
     cq.dequeue();
- 
+
     return 0;
 }
